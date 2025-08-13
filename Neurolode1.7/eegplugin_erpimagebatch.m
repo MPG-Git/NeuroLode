f17e459eeae610e0fb429e75821d89a5da22cf28
MPG-Git <matthewpgunn@gmail.com>
2025-08-13 18:12:55 -0500

    Test

--- Neurolode1.7/eegplugin_erpimagebatch.m
+++
@@ -1,6 +0,0 @@
-function vers = eegplugin_erpimagebatch(fig, ~, ~)
-    vers = '1.0';
-    menu = findobj(fig, 'tag', 'plot');
-    uimenu(menu, 'label', 'ERP Image Batch Tool', ...
-           'callback', 'pop_erpimage_batch(EEG);');
-end
